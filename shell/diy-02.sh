22f362492be03b1723cea897c2399c14478274cc
cddcx <84568016+cddcx@users.noreply.github.com>
2022-02-12 17:01:45 +0800

    Update diy-02.sh

--- shell/diy-02.sh
+++ shell/diy-02.sh
@@ -4,7 +4,7 @@
 #sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
 # 修改密码
-sed -i 's/root::0:0:99999:7:::/root:$1$SOP5eWTA$fJV8ty3QohO0chErhlxCm1:18775:0:99999:7:::/g' openwrt/package/base-files/files/etc/shadow
+sed -i 's/root::0:0:99999:7:::/root:$1$SOP5eWTA$fJV8ty3QohO0chErhlxCm1:18775:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # 修改默认主题
-sed -i 's/bootstrap/argon/' openwrt/feeds/luci/collections/luci/Makefile
+sed -i 's/bootstrap/argon/' feeds/luci/collections/luci/Makefile
