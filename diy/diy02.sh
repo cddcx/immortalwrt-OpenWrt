d818fd7be51e97e5d6bd53bc92ee71ade78adcfb
cddcx <84568016+cddcx@users.noreply.github.com>
2022-02-23 15:17:03 +0800

    Update diy02.sh

--- diy/diy02.sh
+++ diy/diy02.sh
@@ -11,6 +11,7 @@ sed -i 's/bootstrap/argon/' feeds/luci/collections/luci/Makefile
 
 # 整理
 #rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-opentopd
 
 # 修改网络
 #sed -i 's/eth0/eth0 eth2 eth3/' package/base-files/files/etc/board.d/99-default_network
