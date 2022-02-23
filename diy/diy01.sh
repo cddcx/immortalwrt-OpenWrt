19811b940a99c80a7c442f8b3541822926f16322
cddcx <84568016+cddcx@users.noreply.github.com>
2022-02-23 16:45:38 +0800

    Update diy01.sh

--- diy/diy01.sh
+++ diy/diy01.sh
@@ -28,6 +28,7 @@ git clone https://github.com/sirpdboy/luci-theme-opentopd package/luci-theme-ope
 # luci-theme-argon改版主题
 #git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+#rm -rf package/luci-theme-argon/README* package/luci-theme-argon/Screenshots/
 #sed -i 's/luci-theme-argon-18.06/luci-theme-argon/g' package/luci-theme-argon/Makefile
 
 # ikoolproxy去广告插件
