f395367100c5ce63fc0b34ec0c31c7261682558c
cddcx <84568016+cddcx@users.noreply.github.com>
2022-02-23 16:43:29 +0800

    Update diy1.sh

--- diy/diy1.sh
+++ diy/diy1.sh
@@ -24,6 +24,7 @@ sed -i 's/("iStore"), 31/("应用商店"), 61/g' package/luci-app-store/luasrc/c
 
 # luci-theme-argon改版主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+rm -rf package/luci-theme-argon/README* package/luci-theme-argon/Screenshots/
 #sed -i 's/luci-theme-argon-18.06/luci-theme-argon/g' package/luci-theme-argon/Makefile
 
 # ikoolproxy去广告插件
