f2d6ffa8745770a936732e2179c5785a76fc1d2d
cddcx <84568016+cddcx@users.noreply.github.com>
2022-02-23 14:45:38 +0800

    Update diy01.sh

--- diy/diy01.sh
+++ diy/diy01.sh
@@ -23,7 +23,7 @@ rm -rf package/luci-app-store/.svn
 sed -i 's/("iStore"), 31/("应用商店"), 61/g' package/luci-app-store/luasrc/controller/store.lua
 
 # luci-theme-argon改版主题
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #sed -i 's/luci-theme-argon-18.06/luci-theme-argon/g' package/luci-theme-argon/Makefile
 
