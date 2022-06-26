c7a631d054f93a6bfe4e5fbc01704291489e7780
cddcx <84568016+cddcx@users.noreply.github.com>
2022-06-26 09:18:02 +0800

    Update diy2.sh

--- diy/diy2.sh
+++ diy/diy2.sh
@@ -12,6 +12,13 @@ sed -i 's/bootstrap/opentopd/' feeds/luci/collections/luci/Makefile
 # 整理
 #rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/themes/luci-theme-argon*
+rm -rf feeds/luci/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-serverchan
+rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/packages/net/mosdns
+rm -rf package/openwrt-passwall/v2ray-geodata
+rm -rf package/ssr/v2ray-geodata
 
 # 修改网络
 #sed -i 's/eth0/eth0 eth2 eth3/' package/base-files/files/etc/board.d/99-default_network
