c1c62068db5396f9ad94ffd91a09ca53fb460380
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-13 14:28:02 +0800

    Update diy01.sh

--- 21.02/diy01.sh
+++ 21.02/diy01.sh
@@ -4,4 +4,4 @@
 sed -i 's/root::0:0:99999:7:::/root:$1$xUooaZpA$6zs50xt4ac9sJXiYpycT3\/:19338:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 #酷友社开发的Openwrt插件：DDNSTO远程穿透、易有云存储端、iStore、QuickStart便捷首页
-git clone https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/linkease package/linkease
+svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/linkease package/linkease
