4410bb2e764a695cdefea93da2344ad11e59d342
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-16 12:44:59 +0800

    Update diy01.sh

--- 21.02/diy01.sh
+++ 21.02/diy01.sh
@@ -3,9 +3,6 @@
 #修改密码
 sed -i 's/root::0:0:99999:7:::/root:$1$xUooaZpA$6zs50xt4ac9sJXiYpycT3\/:19338:0:99999:7:::/g' package/base-files/files/etc/shadow
 
-rm -rf feeds.conf.default
-svn export https://github.com/openwrt/openwrt/trunk/feeds.conf.default feeds.conf.default
-
 #酷友社开发的Openwrt插件：DDNSTO远程穿透、易有云存储端、iStore、QuickStart便捷首页
 svn co https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/linkease package/linkease
 svn co https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/jerrykuku/luci-theme-argon package/luci-theme-argon
