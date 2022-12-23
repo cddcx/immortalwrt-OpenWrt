cdce6b7ca58fbd471e77ac403ccea42af9579acf
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-23 18:59:29 +0800

    Update diy1.sh

--- 21.02/diy1.sh
+++ 21.02/diy1.sh
@@ -4,8 +4,10 @@
 sed -i 's/root::0:0:99999:7:::/root:$1$xUooaZpA$6zs50xt4ac9sJXiYpycT3\/:19338:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 #酷友社开发的Openwrt插件：DDNSTO远程穿透、易有云存储端、iStore、QuickStart便捷首页
-svn co https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/linkease package/linkease
-#svn co https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/jerrykuku/luci-theme-argon package/luci-theme-argon
+svn co https://github.com/linkease/nas-packages package/nas
+svn co https://github.com/linkease/nas-packages-luci package/nas_luci
+git clone https://github.com/linkease/istore.git package/istore
+git clone https://github.com/linkease/istore-ui.git package/istore-ui
 
 ## 修改include/target.mk
 sed -i 's/luci-app-filetransfer/luci-app-openclash luci-app-istorex/g' include/target.mk
