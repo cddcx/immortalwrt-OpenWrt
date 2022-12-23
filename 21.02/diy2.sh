e4b1f3630d5022da1059626aac4ab0ed2e2e0fee
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-23 19:05:46 +0800

    Update diy2.sh

--- 21.02/diy2.sh
+++ 21.02/diy2.sh
@@ -8,26 +8,26 @@
 #sed -i 's/192.168.1.1/192.168.100.101/g' package/base-files/files/bin/config_generate
 
 ##取消bootstrap为默认主题
-#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
 
 ##把nas-packages-luci的zh-cn替换成zh_Hans
-sed -i 's/("QuickStart")/("首页")/g' package/linkease/nas-packages-luci/luci-app-quickstart/luasrc/controller/quickstart.lua
-sed -i 's/("NetworkGuide")/("向导")/g' package/linkease/nas-packages-luci/luci-app-quickstart/luasrc/controller/quickstart.lua
-sed -i 's/("RAID")/("磁盘阵列")/g' package/linkease/nas-packages-luci/luci-app-quickstart/luasrc/controller/quickstart.lua
-sed -i 's/("NetworkPort")/("网口配置")/g' package/linkease/nas-packages-luci/luci-app-quickstart/luasrc/controller/quickstart.lua
+sed -i 's/("QuickStart")/("首页")/g' package/nas_luci/luci-app-quickstart/luasrc/controller/quickstart.lua
+sed -i 's/("NetworkGuide")/("向导")/g' package/nas_luci/luci-app-quickstart/luasrc/controller/quickstart.lua
+sed -i 's/("RAID")/("磁盘阵列")/g' package/nas_luci/luci-app-quickstart/luasrc/controller/quickstart.lua
+sed -i 's/("NetworkPort")/("网口配置")/g' package/nas_luci/luci-app-quickstart/luasrc/controller/quickstart.lua
 #cp -af feeds/xiangfeidexiaohuo/linkease/nas-packages-luci/luci-app-quickstart/po/zh-cn package/nas-packages-luci/luci-app-quickstart/po/zh_Hans
 
 ## 删除软件
 #rm -rf feeds/luci/applications/luci-app-disk*
 #rm -rf package/feeds/luci/luci-app-disk*
-rm -rf feeds/luci/applications/luci-app-attendedsysupgrade
+#rm -rf feeds/luci/applications/luci-app-attendedsysupgrade
 rm -rf feeds/luci/applications/luci-app-docker*
-rm -rf package/feeds/luci/luci-app-attendedsysupgrade
+#rm -rf package/feeds/luci/luci-app-attendedsysupgrade
 rm -rf package/feeds/luci/luci-app-docker*
 rm -rf package/feeds/luci/luci-lib-docker
-rm -rf feeds/packages/utils/attendedsysupgrade*
+#rm -rf feeds/packages/utils/attendedsysupgrade*
 rm -rf feeds/packages/utils/docker*
 rm -rf package/feeds/packages/docker*
 rm -rf feeds/luci/applications/luci-app-smartdns
