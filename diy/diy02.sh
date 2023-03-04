f0d4d094fd0c041d2f68f04e3bbe1614a9da96a6
cddcx <84568016+cddcx@users.noreply.github.com>
2023-03-04 18:01:12 +0800

    Update diy02.sh

--- diy/diy02.sh
+++ diy/diy02.sh
@@ -22,8 +22,8 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-l
 #svn export https://github.com/coolsnowwolf/lede/trunk/target/linux/x86/Makefile target/linux/x86/
 #svn export https://github.com/coolsnowwolf/lede/trunk/package/network/config/firewall/makefile package/network/config/firewall/
 #svn export https://github.com/coolsnowwolf/lede/trunk/package/lean package/lean
-sed -i '/luci-app-cpufreq/d' include/target.mk
-sed -i 's/default-settings-chn/default-settings-chn luci/g' include/target.mk
+#sed -i '/luci-app-cpufreq/d' include/target.mk
+sed -i 's/default-settings-chn/autocore-x86 default-settings-chn luci/g' include/target.mk
 sed -i 's/automount/autocore-x86 default-settings-chn luci luci-app-ssr-plus luci-app-openclash luci-app-udpxy luci-app-upnp/g' target/linux/x86/Makefile
 
 # 整理
