694b56478126dc867e3b1600b7e48fa0751c7e0f
cddcx <84568016+cddcx@users.noreply.github.com>
2023-03-08 23:02:12 +0800

    Update diy-master.sh

--- diy/diy-master.sh
+++ diy/diy-master.sh
@@ -36,7 +36,7 @@
 #rm -rf package/luci-theme-opentopd/README* package/luci-theme-opentopd/doc/
 
 sed -i 's/kmod-ipt-offload/kmod-ipt-offload block-mount coremark kmod-nf-nathelper kmod-nft-tproxy kmod-nf-nathelper-extra kmod-ipt-raw/g' include/target.mk
-sed -i 's/ip6tables/ip6tables ipv6helper kmod-inet-diag default-settings luci luci-newapi/g' include/target.mk
+sed -i 's/ip6tables/ip6tables ipv6helper kmod-inet-diag default-settings-chn luci luci-newapi/g' include/target.mk
 sed -i "s/automount/autocore-x86 usbutils pciutils lm-sensors-detect fdisk lsblk \
 luci-app-firewall luci-app-opkg luci-app-ssr-plus luci-app-udpxy luci-app-upnp/" target/linux/x86/Makefile
 # luci-theme-argon改版主题
