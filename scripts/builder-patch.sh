24eecb6f74fd761718b91ba7b1c4b521daedc54e
cddcx <84568016+cddcx@users.noreply.github.com>
2023-04-02 10:57:49 +0800

    Create builder-patch.sh

---
+++ scripts/builder-patch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Start Builder Patch !"
+echo "Current Path: $PWD"
+
+cd $GITHUB_WORKSPACE/$VENDOR-imagebuilder-$VERSION-x86-64.Linux-x86_64 || exit
+
+# Remove redundant default packages
+sed -i "/luci-app-cpufreq/d" include/target.mk
+
+# Force opkg to overwrite files
+sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrite/" Makefile
+
+# Not generate ISO images for it is too big
+sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
+
+# Not generate VHDX images
+sed -i "s/CONFIG_VHDX_IMAGES=y/# CONFIG_VHDX_IMAGES is not set/" .config
