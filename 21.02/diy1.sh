dd7cb00c79f3e11fd6346d23f25e3a1b837448d0
cddcx <84568016+cddcx@users.noreply.github.com>
2023-04-04 13:17:38 +0800

    Update diy1.sh

--- 21.02/diy1.sh
+++ 21.02/diy1.sh
@@ -13,9 +13,9 @@ sed -i 's/luci-app-turboacc/luci-app-upnp/g' include/target.mk
 sed -i 's/automount/luci-app-openclash luci-app-store/g' target/linux/x86/Makefile
 
 ## luci-app-xray
-svn co https://github.com/yichya/luci-app-xray package/luci-app-xray
-svn co https://github.com/yichya/openwrt-xray package/openwrt-xray
-svn co https://github.com/xiechangan123/luci-i18n-xray-zh-cn package/luci-i18n-xray-zh-cn
+git clone https://github.com/yichya/luci-app-xray.git package/luci-app-xray
+git clone https://github.com/yichya/openwrt-xray.git package/openwrt-xray
+git clone https://github.com/xiechangan123/luci-i18n-xray-zh-cn.git package/luci-i18n-xray-zh-cn
 
 ## luci-app-openclash
 #svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
