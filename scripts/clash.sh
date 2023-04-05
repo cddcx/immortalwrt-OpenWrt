9835454678ca607d80a85b89afef6421b6660428
cddcx <84568016+cddcx@users.noreply.github.com>
2023-04-05 20:40:59 +0800

    Update clash.sh

--- scripts/clash.sh
+++ scripts/clash.sh
@@ -5,7 +5,7 @@ echo "Current Path: $PWD"
 
 mkdir -p files/etc/openclash/core
 cd files/etc/openclash/core || (echo "Clash core path does not exist! " && exit)
-wget https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/dev/clash-linux-amd64.tar.gz
+wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz
 tar -zxvf clash-linux-amd64.tar.gz
 mv clash clash_meta
 rm -rf clash-linux-amd64.tar.gz
