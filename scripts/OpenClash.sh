81638569a43c4c95363d835bd6adf1fa647759dc
cddcx <84568016+cddcx@users.noreply.github.com>
2023-04-06 21:57:12 +0800

    Update OpenClash.sh

--- scripts/OpenClash.sh
+++ scripts/OpenClash.sh
@@ -3,16 +3,9 @@
 echo "Start Clash Core Download !"
 echo "Current Path: $PWD"
 
-# 恢复OpenClash备份
-mkdir -p files/etc/openclash
-cp -rf scripts/Backup-OpenClash*.tar.gz files/etc/openclash
-#chmod +x files/etc/openclash
-cd files/etc/openclash
-tar -zxvf Backup-OpenClash*.tar.gz
-rm -rf Backup-OpenClash*.tar.gz
 # 下载OpenClash核心
-mkdir -p core
-cd core || (echo "Clash core path does not exist! " && exit)
+mkdir -p files/etc/openclash/core
+cd files/etc/openclash/core || (echo "Clash core path does not exist! " && exit)
 wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz
 tar -zxvf clash-linux-amd64.tar.gz
 mv clash clash_meta
