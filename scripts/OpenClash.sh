49917a76c86c7f53b8603fdabb1e37d91c9826fd
cddcx <84568016+cddcx@users.noreply.github.com>
2023-08-19 15:02:34 +0800

    Update OpenClash.sh

--- scripts/OpenClash.sh
+++ scripts/OpenClash.sh
@@ -7,16 +7,13 @@ mkdir -p files/etc/openclash/core
 #cd files/etc/openclash/core
 cd files/etc/openclash/core || (echo "Clash core path does not exist! " && exit)
 
-# 下载OpenClash的dev核心
-wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz
-tar -zxvf clash-linux-amd64.tar.gz
-mv clash clash_dev
-rm -rf clash-linux-amd64.tar.gz
-
 # 下载OpenClash的meta核心
 wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz
 tar -zxvf clash-linux-amd64.tar.gz
 mv clash clash_meta
 rm -rf clash-linux-amd64.tar.gz
 
-mv clash_dev clash
+# 下载OpenClash的dev核心
+wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz
+tar -zxvf clash-linux-amd64.tar.gz
+rm -rf clash-linux-amd64.tar.gz
