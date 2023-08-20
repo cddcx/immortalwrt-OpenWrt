e8bc86e086500f2ab53fb3004f275af5087f4302
cddcx <84568016+cddcx@users.noreply.github.com>
2023-08-20 08:23:33 +0800

    Update diy1.sh

--- 21.02/diy1.sh
+++ 21.02/diy1.sh
@@ -7,17 +7,17 @@ sed -i 's/root::0:0:99999:7:::/root:$1$xUooaZpA$6zs50xt4ac9sJXiYpycT3\/:19338:0:
 #svn co https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/linkease package/linkease
 
 # 软件中心istore
-git clone https://github.com/linkease/istore.git package/istore
-git clone https://github.com/linkease/istore-ui.git package/istore-ui
+#git clone https://github.com/linkease/istore.git package/istore
+#git clone https://github.com/linkease/istore-ui.git package/istore-ui
 #sed -i 's/luci-lib-ipkg/luci-base/g' package/istore/luci/luci-app-store/Makefile
-sed -i 's/("iStore"), 31/("应用商店"), 61/g' package/istore/luci/luci-app-store/luasrc/controller/store.lua
+#sed -i 's/("iStore"), 31/("应用商店"), 61/g' package/istore/luci/luci-app-store/luasrc/controller/store.lua
 
 sed -i 's/iptables/iptables ip6tables kmod-ipt-offload odhcp6c odhcpd-ipv6only/g' include/target.mk
 sed -i 's/ipv6helper/ipv6helper block-mount default-settings-chn kmod-ipt-raw kmod-nf-nathelper kmod-nf-nathelper-extra luci luci-compat luci-lib-base luci-lib-fs luci-lib-ipkg/g' include/target.mk
 sed -i 's/luci-app-filetransfer/luci-app-udpxy/g' include/target.mk
 sed -i 's/luci-app-turboacc/luci-app-upnp/g' include/target.mk
 #sed -i 's/automount/luci-app-Store luci-app-vssr/g' target/linux/x86/Makefile
-sed -i 's/automount/luci-app-Store luci-app-openclash/g' target/linux/x86/Makefile
+sed -i 's/automount/luci-app-openclash/g' target/linux/x86/Makefile
 
 ## luci-app-vssr依赖
 #svn co https://github.com/kenzok8/small/trunk/xray-core package/xray-core
