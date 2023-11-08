cdacbd9d20462ab8c5a3bf4a3bd5a50de02e7317
cddcx <84568016+cddcx@users.noreply.github.com>
2023-11-08 16:08:00 +0800

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -9,8 +9,8 @@ sed -i "s/kmod-nft-offload/kmod-nft-offload kmod-nft-tproxy/" include/target.mk
 # 修改target/linux/x86/Makefile
 sed -i 's/automount/autocore default-settings-chn ipset luci luci-compat luci-app-udpxy luci-app-upnp luci-app-passwall2 luci-app-v2raya/g' target/linux/x86/Makefile
 
-# luci-app-ssr-plus
-#git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+# luci-theme-argon改版主题
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
 # 添加软件源
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
