f8f59b4e76a8942fa62b6973dd11f84118be51da
cddcx <84568016+cddcx@users.noreply.github.com>
2023-11-08 16:00:51 +0800

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-#========================================================================================================================
-# https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt
-# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/openwrt/openwrt / Branch: main
-#========================================================================================================================
 
-# Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# 修改内核
+sed -i 's/PATCHVER:=*.*/PATCHVER:=6.1/g' target/linux/x86/Makefile
 
-# other
-# rm -rf package/utils/{ucode,fbtest}
+# 修改include/target.mk
+sed -i "s/kmod-nft-offload/kmod-nft-offload kmod-nft-tproxy/" include/target.mk
+
+# 修改target/linux/x86/Makefile
+sed -i 's/automount/autocore default-settings-chn ipset luci luci-compat luci-app-udpxy luci-app-upnp luci-app-passwall2 luci-app-v2raya/g' target/linux/x86/Makefile
+
+# luci-app-ssr-plus
+#git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+
+# 添加软件源
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
